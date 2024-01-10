4c850211c3595b95846212d823f5fbd589543408
metehancaliskan <metehan.caliskan@ug.bilkent.edu.tr>
2024-01-10 19:09:52 +0300

    public bets completed, import credits.aleo stuck, apis check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,29 +5,59 @@ import TheirTurn from '@components/TheirTurn';
 import YourTurn from '@components/YourTurn';
 import { useGameStore } from '@state/gameStore';
 import { useNewGameStore } from './NewGame/store';
-import { requestCreateEvent,getEvent ,useAccount, GetEventResponse, getEvents,GetEventsResponse, EventsFilter} from '@puzzlehq/sdk';
+import { requestCreateEvent,getEvent ,useAccount, GetEventResponse, getAccount, GetSelectedAccountResponse, PuzzleAccount, GetEventsResponse, EventsFilter} from '@puzzlehq/sdk';
 import { EventType } from '@puzzlehq/types';
 import { useState } from 'react';
 
 function Home() {
+
   const [yourTurn, theirTurn, totalBalance] = useGameStore((state) => [
     state.yourTurn,
     state.theirTurn,
     state.totalBalance,
   ]);
+  // const [account, setAccount] = useState<PuzzleAccount | undefined>();
+
   const [initialize] = useNewGameStore((state) => [state.initialize]);
   const { account } = useAccount();
-  const record1 = { player: 'aleo19jp6rwmzq4m2nj32h8fmtecl4hyclajxnzx4plksgsp3qtcsaygqdk68dr'};
-
+  const record1 = {bet_amount:'20u64', bet:'2u32'};
   const navigate = useNavigate();
+
+const testGetAccount = async () => {
+  console.log(account?.address);
+}
+
+const testGetMap = async () => {
+  // Define the URL of the endpoint
+  const url = `https://api.explorer.aleo.org/v1/testnet3/program/cassino_game_test_fp.aleo/mapping/account/${account?.address}`;
+  
+
+  try {
+      // Use the fetch API to send a GET request
+      const response = await fetch(url);
+
+      // Check if the response is ok (status in the range 200-299)
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Process the JSON data
+      const data = await response.json();
+
+      console.log(data); // Log or process the data as needed
+  } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
 
 const testEvent = async () => {
 
 const createEventResponse = await requestCreateEvent({
   type: EventType.Execute,
-  programId:"cassino_game_test_ekim.aleo",
-  functionId:"random_number_generate",
-  fee:2,
+  programId:"cassino_game_test_fp.aleo",
+  functionId:"make_dozen_bet_public",
+  fee:1.5,
   inputs:Object.values(record1),
   
 })
@@ -42,10 +72,10 @@ if(createEventResponse.error){
 const getEventResponseTest = async () => {
   try {
     const response: GetEventResponse = await getEvent({
-      id: "659e3ec8ce397a383701b4ae",
-      address: "aleo19jp6rwmzq4m2nj32h8fmtecl4hyclajxnzx4plksgsp3qtcsaygqdk68dr"
+      id: "659eb104ce397a383702322c",
+      address: account?.address
     });
-    alert(JSON.stringify(response.event!));
+    alert(JSON.stringify(response.event));
   } catch (e) {
     alert((e as Error).message);
   } finally {
@@ -61,13 +91,13 @@ const filter: EventsFilter  = {
 }
 
 
-try {
-  const events:GetEventsResponse = await getEvents(filter)
+// try {
+//   // const events:GetEventsResponse = await getEvents(filter)
 
-  console.log(events)
-} catch (error) {
+//   console.log(events)
+// } catch (error) {
   
-}
+// }
 
 }
 
@@ -87,9 +117,10 @@ try {
         >
           NEW GAME
         </Button>
-        <Button color='green' onClick={()=>testEvent()}>TEST </Button>
+        <Button color='green' onClick={()=>testGetAccount()}>TEST Account </Button>
+        <Button color='green' onClick={()=>testEvent()}>TEST Event </Button>
         <Button color='green' onClick={()=>getEventResponseTest()}>get event </Button>
-        <Button color='green' onClick={()=>getAllEvents()}>get all events </Button>
+        <Button color='green' onClick={()=>testGetMap()}>get key from map </Button>
 
 
         {yourTurn.length > 0 && <YourTurn games={yourTurn} />}
@@ -109,6 +140,6 @@ try {
       </div>
     </div>
   );
-}
+    }
 
 export default Home;
