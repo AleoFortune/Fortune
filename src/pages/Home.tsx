fc925d59370c0724358610ccbb1b4c84b7989ffa
ekimcem <ekim@bronixengineering.com>
2024-01-11 18:39:05 +0300

    refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '@components/Button';
-import TotalWinnings from '@components/TotalWinnings';
-import TheirTurn from '@components/TheirTurn';
-import YourTurn from '@components/YourTurn';
-import { useGameStore } from '@state/gameStore';
-import { useNewGameStore } from './NewGame/store';
-import { requestCreateEvent,getEvent ,useAccount, GetEventResponse, getAccount, GetSelectedAccountResponse, PuzzleAccount, GetEventsResponse, EventsFilter} from '@puzzlehq/sdk';
-import { EventType } from '@puzzlehq/types';
-import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import Button from "@components/Button";
+import TotalWinnings from "@components/TotalWinnings";
+import TheirTurn from "@components/TheirTurn";
+import YourTurn from "@components/YourTurn";
+import { useGameStore } from "@state/gameStore";
+import { useNewGameStore } from "./NewGame/store";
+import {
+  requestCreateEvent,
+  getEvent,
+  useAccount,
+  GetEventResponse,
+  GetEventsResponse,
+  EventsFilter,
+  getEvents,
+} from "@puzzlehq/sdk";
+import { EventType } from "@puzzlehq/types";
 
 function Home() {
-
   const [yourTurn, theirTurn, totalBalance] = useGameStore((state) => [
     state.yourTurn,
     state.theirTurn,
@@ -20,141 +26,186 @@ function Home() {
 
   const [initialize] = useNewGameStore((state) => [state.initialize]);
   const { account } = useAccount();
-  const record1 = {bet_amount:'20u64', bet:'2u32'};
+  const record1 = { bet_amount: "20u64", bet: "2u32" };
   const navigate = useNavigate();
-  const input = {reciever:'aleo1696yxs062yrm0nmvflwcp7zjy0l8l4w8gpr3wn0ql3nttu54ayqsuxqus5', amount: '10u64'}
+  const input = {
+    reciever: "aleo1696yxs062yrm0nmvflwcp7zjy0l8l4w8gpr3wn0ql3nttu54ayqsuxqus5",
+    amount: "10u64",
+  };
 
-const testGetAccount = async () => {
-  console.log(account?.address);
-}
+  const testGetAccount = async () => {
+    console.log(account?.address);
+  };
 
-
-
-const testGetMapBalance = async () => {
-  const url = `https://api.explorer.aleo.org/v1/testnet3/program/cassino_game_test_fp.aleo/mapping/account/${account?.address}`;
-  try {
+  const testGetMapBalance = async () => {
+    const url = `https://api.explorer.aleo.org/v1/testnet3/program/cassino_game_test_fp.aleo/mapping/account/${account?.address}`;
+    try {
       const response = await fetch(url);
       if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
       console.log(data); // Log or process the data as needed
-  } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-  }
-}
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
 
-
-const testEvent = async () => {
-
-const createEventResponse = await requestCreateEvent({
-  type: EventType.Execute,
-  programId:"cassino_game_test_fp.aleo",
-  functionId:"make_dozen_bet_public",
-  fee:1.5,
-  inputs:Object.values(record1),
-  
-})
-if(createEventResponse.error){
-  alert(createEventResponse.error)
-}else {
-  alert(createEventResponse.eventId)
-}
-
-}
-
-
-const testTransferPublic = async () => {
-
-  const createEventResponse = await requestCreateEvent({
-    type: EventType.Execute,
-    programId:"credits.aleo",
-    functionId:"transfer_public",
-    fee:3,
-    inputs:Object.values(input),
-    
-  })
-  if(createEventResponse.error){
-    alert(createEventResponse.error)
-  }else {
-    alert(createEventResponse.eventId)
-  }
-  
-  }
-
-const getEventResponseTest = async () => {
-  try {
-    const response: GetEventResponse = await getEvent({
-      id: "659eb104ce397a383702322c",
-      address: account?.address
+  const testEvent = async () => {
+    const createEventResponse = await requestCreateEvent({
+      type: EventType.Execute,
+      programId: "cassino_game_test_fp.aleo",
+      functionId: "make_dozen_bet_public",
+      fee: 1.5,
+      inputs: Object.values(record1),
     });
-    alert(JSON.stringify(response.event));
-  } catch (e) {
-    alert((e as Error).message);
-  } finally {
-  }
-  
-}
+    if (createEventResponse.error) {
+      alert(createEventResponse.error);
+    } else {
+      alert(createEventResponse.eventId);
+    }
+  };
 
-const getAllEvents = async( ) => {
-const filter: EventsFilter  = {
-  type: EventType.Execute,
-  programId:"cassino_game_test_ekim.aleo",
-  functionId:"random_number_generate",
-}
+  const depositCredit = async () => {
+    if (!account) {
+      alert("no account");
+      return;
+    }
+    const fields = { receiver: account?.address, amount: "100u64" };
 
+    const createEventResponse = await requestCreateEvent({
+      type: EventType.Execute,
+      programId: "cassino_game_test_fp.aleo",
+      functionId: "deposit_public",
+      fee: 1.5,
+      inputs: Object.values(fields),
+    });
+    if (createEventResponse.error) {
+      alert(JSON.stringify(createEventResponse));
+    } else {
+      alert(JSON.stringify(createEventResponse));
+      console.log(createEventResponse);
+    }
+  };
 
-// try {
-//   // const events:GetEventsResponse = await getEvents(filter)
+  const testTransferPublic = async () => {
+    const createEventResponse = await requestCreateEvent({
+      type: EventType.Execute,
+      programId: "credits.aleo",
+      functionId: "transfer_public",
+      fee: 3,
+      inputs: Object.values(input),
+    });
+    if (createEventResponse.error) {
+      alert(createEventResponse.error);
+    } else {
+      alert(createEventResponse.eventId);
+    }
+  };
 
-//   console.log(events)
-// } catch (error) {
-  
-// }
+  const playKumar = async () => {
+    let playInput = { bet_amount: "10u64", bet: "0u32" };
 
-}
+    const createEventResponse = await requestCreateEvent({
+      type: EventType.Execute,
+      programId: "cassino_game_test_fp.aleo",
+      functionId: "make_odd_even_bet_public",
+      fee: 1.5,
+      inputs: Object.values(playInput),
+    });
+    if (createEventResponse.error) {
+      alert(JSON.stringify(createEventResponse.error));
+    } else {
+      alert(JSON.stringify(createEventResponse.eventId));
+    }
+  };
 
+  const getEventResponseTest = async () => {
+    try {
+      const response: GetEventResponse = await getEvent({
+        id: "659eb104ce397a383702322c",
+        address: account?.address,
+      });
+      alert(JSON.stringify(response.event));
+    } catch (e) {
+      alert((e as Error).message);
+    } finally {
+    }
+  };
+
+  const getAllEvents = async () => {
+    const filter: EventsFilter = {
+      type: EventType.Execute,
+      programId: "cassino_game_test_fp.aleo",
+      functionId: "make_odd_even_bet_public",
+    };
+
+    try {
+      const events: GetEventsResponse = await getEvents(filter);
+
+      console.log(events);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className='flex h-full flex-col justify-between '>
-      <div className='flex w-full flex-col gap-4 px-1'>
+    <div className="flex h-full flex-col justify-between ">
+      <div className="flex w-full flex-col gap-4 px-1">
         <TotalWinnings amount={totalBalance} />
         <Button
-          color='yellow'
+          color="yellow"
           onClick={() => {
             if (!account) return;
             initialize(account?.address);
-            navigate('/new-game');
+            navigate("/new-game");
           }}
           disabled={!account}
         >
           NEW GAME
         </Button>
-        <Button color='green' onClick={()=>testGetAccount()}>TEST Account </Button>
-        <Button color='green' onClick={()=>testEvent()}>TEST Event </Button>
-        <Button color='green' onClick={()=>getEventResponseTest()}>get event </Button>
-        <Button color='green' onClick={()=>testGetMapBalance()}>get key from map </Button>
-        <Button color='green' onClick={()=>testTransferPublic()}>transfer public </Button>
-
-
+        <Button color="green" onClick={() => testGetAccount()}>
+          TEST Account{" "}
+        </Button>
+        <Button color="green" onClick={() => testEvent()}>
+          TEST Event{" "}
+        </Button>
+        <Button color="green" onClick={() => getEventResponseTest()}>
+          get event{" "}
+        </Button>
+        <Button color="green" onClick={() => getAllEvents()}>
+          GET ALL EVENTS
+        </Button>
+        <Button color="green" onClick={() => testGetMapBalance()}>
+          get key from map{" "}
+        </Button>
+        <Button color="green" onClick={() => testTransferPublic()}>
+          transfer public{" "}
+        </Button>
+        <Button color="green" onClick={() => depositCredit()}>
+          Deposit 100 Credit
+        </Button>
+        <Button color="green" onClick={() => playKumar()}>
+          Rastgele{" "}
+        </Button>
 
         {yourTurn.length > 0 && <YourTurn games={yourTurn} />}
         {theirTurn.length > 0 && <TheirTurn games={theirTurn} />}
         {yourTurn.length === 0 && theirTurn.length === 0 && (
-          <p className='self-center font-semibold'>
+          <p className="self-center font-semibold">
             No ongoing games, start one with a friend!
           </p>
         )}
       </div>
-      <div className='mt-4 px-4 pb-4 text-center'>
-        {' '}
+      <div className="mt-4 px-4 pb-4 text-center">
+        {" "}
         {/* Adding px-4 back here to maintain padding for the bottom button */}
-        <Button color='blue' size='sm'>
+        <Button color="blue" size="sm">
           Past Games
         </Button>
       </div>
     </div>
   );
-    }
+}
 
 export default Home;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,10 +1,10 @@
-import { useConnect, useAccount } from '@puzzlehq/sdk';
-import rightImageSrc from '../assets/alex_mic_left_tilt.png';
-import leftImageSrc from '../assets/alex_mic_right_tilt.png';
-import bottomImageSrc from '../assets/alexbottom.png';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import Button from '@components/Button.js';
+import { useConnect, useAccount } from "@puzzlehq/sdk";
+import rightImageSrc from "../assets/alex_mic_left_tilt.png";
+import leftImageSrc from "../assets/alex_mic_right_tilt.png";
+import bottomImageSrc from "../assets/alexbottom.png";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import Button from "@components/Button.js";
 
 export const Welcome = () => {
   const navigate = useNavigate();
@@ -13,44 +13,41 @@ export const Welcome = () => {
 
   useEffect(() => {
     if (account) {
-      navigate('/');
+      navigate("/");
     }
   }, [account, navigate]);
 
   return (
-    <div className='flex h-full w-full items-stretch justify-between'>
-      <div className='relative flex h-full w-full flex-col items-center justify-center'>
+    <div className="flex h-full w-full items-stretch justify-between">
+      <div className="relative flex h-full w-full flex-col items-center justify-center">
         <img
           src={rightImageSrc}
-          alt='Top-right Alex'
-          className='fixed right-0 top-0 h-full max-h-[18rem] max-w-[50%] object-contain'
+          alt="Top-right Alex"
+          className="fixed right-0 top-0 h-full max-h-[18rem] max-w-[50%] object-contain"
         />
         <img
           src={leftImageSrc}
-          alt='Left Alex'
-          className='fixed left-0 top-1/4 h-full max-h-[20rem] max-w-[50%] -translate-y-20 object-contain'
+          alt="Left Alex"
+          className="fixed left-0 top-1/4 h-full max-h-[20rem] max-w-[50%] -translate-y-20 object-contain"
         />
-        <h1 className='text-24xl z-10 max-w-full overflow-visible whitespace-nowrap text-center font-extrabold leading-[40.56px] tracking-tight text-primary-white'>
-          WHERE'S
-          <br />
-          ALEX?
+        <h1 className="text-24xl z-10 max-w-full overflow-visible whitespace-nowrap text-center font-extrabold leading-[40.56px] tracking-tight text-primary-white">
+          Aleo Fortune
         </h1>
-        <p className='z-10 mb-8 mt-8 max-w-[400px] text-center text-base font-bold tracking-tight text-primary-white'>
-          A thrilling game showcasing the power of Aleo and the Puzzle
-          multiparty privacy stack through a wager between friends!
+        <p className="z-10 mb-8 mt-8 max-w-[400px] text-center text-base font-bold tracking-tight text-primary-white">
+          Connect your Wallet to play !
         </p>
         <Button
-          className='max-w-[250px]'
+          className="max-w-[250px]"
           onClick={connect}
-          color='yellow'
+          color="yellow"
           disabled={loading}
         >
-          {loading ? 'Loading...' : loading ? 'Connecting...' : 'Play!'}
+          {loading ? "Loading..." : loading ? "Connecting..." : "Connect!"}
         </Button>
         <img
           src={bottomImageSrc}
-          alt='Bottom Alex'
-          className='center -translate-y-100 fixed bottom-0 h-full max-h-[12rem] w-3/5 max-w-[35%] transform object-contain'
+          alt="Bottom Alex"
+          className="center -translate-y-100 fixed bottom-0 h-full max-h-[12rem] w-3/5 max-w-[35%] transform object-contain"
         />
       </div>
     </div>
